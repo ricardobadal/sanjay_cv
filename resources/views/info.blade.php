4c94f93565e02c8fa04c447fe8fd52b2322d3421
ricardobadal <ricardobadal@gmail.com>
2022-05-08 14:55:20 +0200

    updated layout

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -1,10 +1,14 @@
-<div>
+<div class="info">
     <h4>Ricardo Sanjay Badal</h4>
     Roepnaam: Sanjay<br>
-    Toppingaheerd<br>
-    9737 TR Groningen<br>
-    <i><a href="mailto:ricardobadal@gmail.com">Email</a></i><br>
-    <i>06 533 06 07</i><br>
+    <address id="myAdr">
+        Toppingaheerd<br>
+        9737 TR Groningen<br>
+    </address>
+    <div>
+        <i class="fas fa-briefcase"><a href="mailto:ricardobadal@gmail.com">Email</a></i><br>
+        <i class="fas fa-phone">06 533 06 07</i></button>
+    </div>
     <br>
     Geboren: 25-Mei-1978 te Paramaribo<br>
     Nationaliteit: Nederlandse<br>
@@ -12,4 +16,20 @@
     <br>
     <i><a href="https://www.linkedin.com/in/rsbadal/">LinkedIn</a></i><br>
     <i><a href="https://github.com/ricardobadal/">Git</a></i><br>
+</div>
+<div class="info">
+    <h4>Vaardigheden:</h4>
+    <ul>
+        <li>Windows 7/10</li>
+        <li>Linux (Ubuntu & Fedora)</li>
+        <li>OS X</li>
+        <li>Microsoft Office</li>
+        <li>Adobe Photoshop</li>
+        <li>HTML & CSS</li>
+        <li>JavaScript & VueJS</li>
+        <li>Composer NPM</li>
+        <li>PHP & Laravel</li>
+        <li>MySQL & PhpMyAdmin</li>
+        <li>Git</li>
+    </ul>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,10 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+        integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -57,6 +61,7 @@
             </div>
         </div>
     </div>
+    <script src="{{ asset('js/main.js') }}" defer></script>
 </body>
 
 </html>
